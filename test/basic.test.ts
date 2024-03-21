fa4ebac97116f0a9df9c55c03569959d54ec94e8
Pooya Parsa <pooya@pi0.io>
2024-03-21 18:14:11 +0100

    test: update snapshot

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -17,7 +17,7 @@ describe('nuxt-shiki', async () => {
   it('ssr works <Shiki> component', async () => {
     const html = await $fetch('/')
     const body = html.match(/<div id="__nuxt">([\s\S]+)<\/div>/)?.[1]
-    expect(body).toMatchInlineSnapshot(`"<pre><code class="shiki min-light" style="background-color:#ffffff;color:#24292eff" tabindex="0"><span class="line"><span style="color:#1976D2">console</span><span style="color:#6F42C1">.log</span><span style="color:#24292EFF">(</span><span style="color:#22863A">'hello'</span><span style="color:#24292EFF">);</span></span></code></pre>"`)
+    expect(body).toMatchInlineSnapshot(`"<pre><code class="shiki min-light" style="background-color:#ffffff;color:#24292eff" tabindex="0"><span class="line"><span style="color:#1976D2">console</span><span style="color:#6F42C1">.log</span><span style="color:#24292EFF">(</span><span style="color:#22863A">'hello'</span><span style="color:#24292EFF">);</span></span></code></pre></div><div id="teleports">"`)
   })
 
   // TODO: Client-side rendering
